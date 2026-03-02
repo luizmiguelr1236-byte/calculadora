cae7b7a344c42f7d2206a7248ab28f433a0fee97
sla <luizmiguel.r1236@gmail.com>
2026-03-02 11:38:26 -0300

    Adicionar arquivos de projeto.

---
+++ calculadora/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace calculadora
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnsomar = new System.Windows.Forms.Button();
+            this.btnsub = new System.Windows.Forms.Button();
+            this.btnmult = new System.Windows.Forms.Button();
+            this.btndiv = new System.Windows.Forms.Button();
+            this.btncomp = new System.Windows.Forms.Button();
+            this.btnparimpar = new System.Windows.Forms.Button();
+            this.btnlimpar = new System.Windows.Forms.Button();
+            this.btnfechar = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.result = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnsomar
+            // 
+            this.btnsomar.Location = new System.Drawing.Point(259, 69);
+            this.btnsomar.Name = "btnsomar";
+            this.btnsomar.Size = new System.Drawing.Size(96, 38);
+            this.btnsomar.TabIndex = 0;
+            this.btnsomar.Text = "Somar";
+            this.btnsomar.UseVisualStyleBackColor = true;
+            this.btnsomar.Click += new System.EventHandler(this.btnsomar_Click);
+            // 
+            // btnsub
+            // 
+            this.btnsub.Location = new System.Drawing.Point(259, 113);
+            this.btnsub.Name = "btnsub";
+            this.btnsub.Size = new System.Drawing.Size(96, 38);
+            this.btnsub.TabIndex = 1;
+            this.btnsub.Text = "subtrair";
+            this.btnsub.UseVisualStyleBackColor = true;
+            this.btnsub.Click += new System.EventHandler(this.btnsub_Click);
+            // 
+            // btnmult
+            // 
+            this.btnmult.Location = new System.Drawing.Point(259, 157);
+            this.btnmult.Name = "btnmult";
+            this.btnmult.Size = new System.Drawing.Size(96, 37);
+            this.btnmult.TabIndex = 2;
+            this.btnmult.Text = "multiplicar";
+            this.btnmult.UseVisualStyleBackColor = true;
+            this.btnmult.Click += new System.EventHandler(this.btnmult_Click);
+            // 
+            // btndiv
+            // 
+            this.btndiv.Location = new System.Drawing.Point(259, 198);
+            this.btndiv.Name = "btndiv";
+            this.btndiv.Size = new System.Drawing.Size(96, 37);
+            this.btndiv.TabIndex = 3;
+            this.btndiv.Text = "dividir";
+            this.btndiv.UseVisualStyleBackColor = true;
+            this.btndiv.Click += new System.EventHandler(this.btndiv_Click);
+            // 
+            // btncomp
+            // 
+            this.btncomp.Location = new System.Drawing.Point(30, 200);
+            this.btncomp.Name = "btncomp";
+            this.btncomp.Size = new System.Drawing.Size(96, 35);
+            this.btncomp.TabIndex = 4;
+            this.btncomp.Text = "Comparar";
+            this.btncomp.UseVisualStyleBackColor = true;
+            this.btncomp.Click += new System.EventHandler(this.btncomp_Click);
+            // 
+            // btnparimpar
+            // 
+            this.btnparimpar.Location = new System.Drawing.Point(132, 200);
+            this.btnparimpar.Name = "btnparimpar";
+            this.btnparimpar.Size = new System.Drawing.Size(91, 35);
+            this.btnparimpar.TabIndex = 5;
+            this.btnparimpar.Text = "Par Ímpar";
+            this.btnparimpar.UseVisualStyleBackColor = true;
+            this.btnparimpar.Click += new System.EventHandler(this.btnparimpar_Click);
+            // 
+            // btnlimpar
+            // 
+            this.btnlimpar.Location = new System.Drawing.Point(30, 256);
+            this.btnlimpar.Name = "btnlimpar";
+            this.btnlimpar.Size = new System.Drawing.Size(96, 39);
+            this.btnlimpar.TabIndex = 6;
+            this.btnlimpar.Text = "Limpar";
+            this.btnlimpar.UseVisualStyleBackColor = true;
+            this.btnlimpar.Click += new System.EventHandler(this.btnlimpar_Click);
+            // 
+            // btnfechar
+            // 
+            this.btnfechar.Location = new System.Drawing.Point(259, 256);
+            this.btnfechar.Name = "btnfechar";
+            this.btnfechar.Size = new System.Drawing.Size(96, 39);
+            this.btnfechar.TabIndex = 7;
+            this.btnfechar.Text = "Fechar";
+            this.btnfechar.UseVisualStyleBackColor = true;
+            this.btnfechar.Click += new System.EventHandler(this.btnfechar_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(79, 69);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(134, 26);
+            this.textBox1.TabIndex = 8;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(79, 111);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(134, 26);
+            this.textBox2.TabIndex = 9;
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Location = new System.Drawing.Point(43, 114);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 23);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "   ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(75, 143);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(144, 20);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "---------------------------";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // result
+            // 
+            this.result.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.result.Location = new System.Drawing.Point(75, 163);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(138, 30);
+            this.result.TabIndex = 12;
+            this.result.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(734, 490);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnfechar);
+            this.Controls.Add(this.btnlimpar);
+            this.Controls.Add(this.btnparimpar);
+            this.Controls.Add(this.btncomp);
+            this.Controls.Add(this.btndiv);
+            this.Controls.Add(this.btnmult);
+            this.Controls.Add(this.btnsub);
+            this.Controls.Add(this.btnsomar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnsomar;
+        private System.Windows.Forms.Button btnsub;
+        private System.Windows.Forms.Button btnmult;
+        private System.Windows.Forms.Button btndiv;
+        private System.Windows.Forms.Button btncomp;
+        private System.Windows.Forms.Button btnparimpar;
+        private System.Windows.Forms.Button btnlimpar;
+        private System.Windows.Forms.Button btnfechar;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label result;
+    }
+}
+
---
+++ calculadora/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calculadora
+{
+    public partial class Form1 : Form
+    {
+        int a, b;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnsomar_Click(object sender, EventArgs e)
+        {
+            a = int.Parse(textBox1.Text);
+            b = int.Parse(textBox2.Text);
+        }
+
+        private void btnsub_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnmult_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btndiv_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btncomp_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnparimpar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnfechar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnlimpar_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
