c795f900eeab24cbce441516c00124f7c7e90539
luizm <luizmiguel.r1236@gmail.com>
2026-04-08 21:06:25 -0300

    funcional

--- calculadora/formularios/calcsuper.cs
+++ calculadora/formularios/calcsuper.cs
@@ -16,12 +16,18 @@ namespace calculadora.formularios
         decimal prinumero;
         string operacao;
         bool limpar;
-        private object ButtonBase;
+        private Dictionary<Button, Color> originalColors = new Dictionary<Button, Color>();
+        private readonly Color pressedColor = Color.FromArgb(190, 190, 190);
+        private Button selectedButton = null;
 
         public calcsuper()
         {
             InitializeComponent();
             this.AcceptButton = btnig;
+            
+            this.KeyPreview = true;
+            RegisterButtonEvents(this);
+            
         }
 
 
@@ -138,6 +144,9 @@ namespace calculadora.formularios
             {
                 botao.Text = e.KeyCode.ToString().Substring(6);
                 digitos(botao, e);
+                Button realBtn = FindButtonByName("btn" + e.KeyCode.ToString().Substring(6));
+                if (realBtn != null)
+                    SelectButton(realBtn);
             }
             switch (e.KeyCode)
             {
@@ -145,24 +154,44 @@ namespace calculadora.formularios
                     botao.Text = "+";
                     operacoes(botao, e);
                     labelvisor.Text = "0";
+                    {
+                        Button realBtn = FindButtonByName("btnmais");
+                        if (realBtn != null) SelectButton(realBtn);
+                    }
                     break;
                 case Keys.Subtract:
                     botao.Text = "-";
                     operacoes(botao, e);
                     labelvisor.Text = "0";
+                    {
+                        Button realBtn = FindButtonByName("btnmenos");
+                        if (realBtn != null) SelectButton(realBtn);
+                    }
                     break;
                 case Keys.Multiply:
                     botao.Text = "X";
                     operacoes(botao, e);
                     labelvisor.Text = "0";
+                    {
+                        Button realBtn = FindButtonByName("btnvezes");
+                        if (realBtn != null) SelectButton(realBtn);
+                    }
                     break;
                 case Keys.Divide:
                     botao.Text = ":";
                     operacoes(botao, e);
                     labelvisor.Text = "0";
+                    {
+                        Button realBtn = FindButtonByName("btndividir");
+                        if (realBtn != null) SelectButton(realBtn);
+                    }
                     break;
                 case Keys.Enter:
                     button1_Click(botao, e);
+                    {
+                        Button realBtn = FindButtonByName("btnig");
+                        if (realBtn != null) SelectButton(realBtn);
+                    }
                     break;
                    
             }
@@ -171,12 +200,98 @@ namespace calculadora.formularios
             {
                 botao.Text = e.KeyCode.ToString().Substring(1);
                 digitos(botao, e);
+                Button realBtn = FindButtonByName("btn" + e.KeyCode.ToString().Substring(1));
+                if (realBtn != null) SelectButton(realBtn);
             }
 
         }
         private void calcsuper_KeyUp(object sender, KeyEventArgs e)
         {
+           
+            if (KeyPreview)
+            {
+               
+                string name = null;
+                if (e.KeyCode >= Keys.NumPad0 && e.KeyCode <= Keys.NumPad9)
+                    name = "btn" + e.KeyCode.ToString().Substring(6);
+                else if (e.KeyCode >= Keys.D0 && e.KeyCode <= Keys.D9)
+                    name = "btn" + e.KeyCode.ToString().Substring(1);
+                else
+                {
+                    switch (e.KeyCode)
+                    {
+                        case Keys.Add: name = "btnmais"; break;
+                        case Keys.Subtract: name = "btnmenos"; break;
+                        case Keys.Multiply: name = "btnvezes"; break;
+                        case Keys.Divide: name = "btndividir"; break;
+                        case Keys.Enter: name = "btnig"; break;
+                    }
+                }
 
+                if (!string.IsNullOrEmpty(name))
+                {
+                    Button btn = FindButtonByName(name);
+                    if (btn != null && originalColors.ContainsKey(btn) && btn != selectedButton)
+                        btn.BackColor = originalColors[btn];
+                }
+            }
+        }
+
+        private void RegisterButtonEvents(Control parent)
+        {
+            foreach (Control c in parent.Controls)
+            {
+                if (c is Button btn)
+                {
+                  
+                    btn.UseVisualStyleBackColor = false;
+                    if (!originalColors.ContainsKey(btn))
+                        originalColors[btn] = Color.White;
+                    btn.BackColor = Color.White;
+
+                    btn.MouseDown += Button_MouseDown;
+                    btn.MouseUp += Button_MouseUp;
+                }
+
+                if (c.HasChildren)
+                    RegisterButtonEvents(c);
+            }
+        }
+
+      
+        private void Button_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (sender is Button btn)
+            {
+                SelectButton(btn);
+            }
+        }
+
+        private void Button_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (sender is Button btn && selectedButton != btn && originalColors.ContainsKey(btn))
+            {
+                btn.BackColor = originalColors[btn];
+            }
+        }
+
+        private void SelectButton(Button btn)
+        {
+            if (btn == null) return;
+
+           
+            if (selectedButton != null && selectedButton != btn && originalColors.ContainsKey(selectedButton))
+                selectedButton.BackColor = originalColors[selectedButton];
+
+            selectedButton = btn;
+            if (!originalColors.ContainsKey(btn))
+                originalColors[btn] = Color.White;
+            btn.BackColor = pressedColor;
+        }
+
+        private Button FindButtonByName(string name)
+        {
+            return this.Controls.Find(name, true).FirstOrDefault() as Button;
         }
 
         private void calcsuper_KeyPress(object sender, KeyPressEventArgs e)
